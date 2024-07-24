bd668dca66cca8c29122a8315e10ebb31295b550
kwhong95 <kwhong95@kakao.com>
2024-07-25 07:11:33 +0900

    nativewind init

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { View, ActivityIndicator } from "react-native";
 
 export default function index() {
   return (
-    <View>
+    <View className="flex-1">
       <ActivityIndicator size="large" color="gray" />
     </View>
   );
