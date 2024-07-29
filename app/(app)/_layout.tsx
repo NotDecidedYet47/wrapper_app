6d551aafb344c45763f82f7d0b9a46cf447e8987
kwhong95 <kwhong95@kakao.com>
2024-07-30 07:14:00 +0900

    Revert "Create Webview Message Handler"
    This reverts commit 16c31b7fceb80e832e33ec9b96140901d49190e1.

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from "expo-router";
 
 function AppLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return <Stack />;
 }
 
 export default AppLayout;
--- app/(app)/home.tsx
+++ app/(app)/home.tsx
@@ -1,21 +1,14 @@
-import { View } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { useAuth } from "@/contexts/AuthContext";
-import { WebView } from "react-native-webview";
 
 function home() {
-  const { user } = useAuth();
+  const { logout } = useAuth();
 
   return (
-    <View className="flex-1 dark:bg-gray-900">
-      <WebView
-        source={{ uri: "http://10.0.2.2:3000" }}
-        allowsBackForwardNavigationGestures={true}
-        pullToRefreshEnabled={true}
-        automaticallyAdjustContentInsets={false}
-        allowsFullscreenVideo={false}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-      />
+    <View>
+      <TouchableOpacity onPress={logout} className="p-4 bg-red-500">
+        <Text>LOGOUT</Text>
+      </TouchableOpacity>
     </View>
   );
 }
